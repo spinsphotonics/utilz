216df787e52599f6d8edaf542f6ffb1b6732fa11
Jesse Lu <jesselu@hollisterphotonics.com>
2023-05-24 06:49:06 +0000

    starting to spin up testing

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -7,8 +7,15 @@ import pytest
 
 
 @pytest.mark.parametrize("domain,m,vals", [((4, 5, 6), 1, (1, 3))])
-@pytest.mark.parametrize("thresh,index,expected",
-                         [((5, 0, 0), (0, 2, 0, 3), 1.5)])
+@pytest.mark.parametrize("thresh,index,expected", [
+    # x-threshold.
+    pytest.param((5, 0, -0.5), (0, 1, 0, 0), 1.0, id="x-Ex-pre"),
+    pytest.param((5, 0, -0.5), (0, 2, 0, 0), 1.5, id="x-Ex-on"),
+    pytest.param((5, 0, -0.5), (0, 3, 0, 0), 3.0, id="x-Ex-post"),
+    pytest.param((4, 0, -0.5), (1, 1, 0, 0), 1.0, id="x-Ey-pre"),
+    pytest.param((4, 0, -0.5), (1, 2, 0, 0), 2.0, id="x-Ey-on"),
+    pytest.param((4, 0, -0.5), (1, 3, 0, 0), 3.0, id="x-Ey-post"),
+])
 def test_simple(domain, m, vals, thresh, index, expected):
   xx, yy, zz = domain
   layer = vals[0] * jnp.ones((2, 2 * m * xx, 2 * m * yy))
@@ -16,9 +23,7 @@ def test_simple(domain, m, vals, thresh, index, expected):
       vals[1])
   out = layers.render(layer,
                       jnp.array([thresh[2]]),
-                      1.0 * (jnp.arange(zz)
-                             [:, None] + jnp.array([[-0.5, 0]])),
-                      1.0 * (jnp.arange(zz)
-                             [:, None] + jnp.array([[0.5, 1]])),
+                      jnp.arange(zz)[:, None] + jnp.array([[-0.5, 0]]),
+                      jnp.arange(zz)[:, None] + jnp.array([[0.5, 1]]),
                       m)
-  assert out[index] == expected
+  assert out[index] == pytest.approx(expected)
